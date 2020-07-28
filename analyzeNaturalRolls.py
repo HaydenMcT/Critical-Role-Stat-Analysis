3eb1a1b6138bdc2668dc90d57f00a8471733eae5
HaydenMcT <50335060+HaydenMcT@users.noreply.github.com>
2020-07-27 19:13:40 -0700

    Added in less conservative tests (ones that don't infer any values that require assuming a d20 is uniformly distributed), still without a statistically significant result. Admittedly, these tests do infer some values by assuming a uniformly distributed d4, and not doing so results in a statistically significant result (!), but it seems biased to disregard all rolls where a d4 was used (because sometimes players only use the d4 after they have seen the d20 roll and when they think the d4 might make a difference)

--- analyzeNaturalRolls.py
+++ analyzeNaturalRolls.py
@@ -1,6 +1,15 @@
 import pandas as pd
 import numpy as np
 import re
+import argparse
+
+parser = argparse.ArgumentParser("Runs Statistical Analysis on Jester's Natural Dice Rolls")
+parser.add_argument("--Remove_Adv", action='store_const', default = False, const=True, help="If specified, this flag causes rolls made at advantage or disadvantage to be disregarded from the statistical analysis, instead of the default behaviour of inferring their value based on the other roll (and assuming a uniform distribution over possible dice values)")
+parser.add_argument("--Only_D4_Ranges", action='store_const', default = False, const=True, help="If specified, only uses roll ranges (manually entered ranges of possible dice values) when those ranges result from a character using a skill like Bless or Guidance that adds a d4 to the roll, and not when those ranges involve an assumption that a d20 is uniformly distributed. Admittedly, Bless and Guidance roll ranges assume a uniform distribution on the d4, but removing those rolls is likely to bias the dataset - sometimes players only roll the extra d4 if they think it will change the result of the d20 roll after they have seen the number on the d20.")
+arguments = parser.parse_args()
+
+remove_adv_disadv_rolls = arguments.Remove_Adv
+only_use_roll_range_for_d4 = arguments.Only_D4_Ranges
 
 X = np.array(pd.read_csv("JesterRollsManualDiceRanges.csv")) #load data
 
@@ -59,10 +68,11 @@ print(d20_rolls.shape)
 for i, roll in enumerate(d20_rolls): #each row of the dataset represents information about a single dice roll
     roll_range = str(roll[nat_roll_range_idx])
     if roll_range != 'nan':
-        #if a roll range is specified, parse the lower and upper bounds
-        lower_bound, upper_bound = recoverBoundsFromRollRange(roll_range)
-        # when roll_range is specified, we take it to denote a discrete uniform distribution on the possible dice roll values ,and update tallies accordingly
-        updateTallyWithDist(lower_bound, upper_bound)
+        if not only_use_roll_range_for_d4 or ('bless' in str(roll[notes_idx]).lower()) or ('guidance' in str(roll[notes_idx]).lower()): #if this roll range involves assuming a d20 is uniformly distributed and the optional argument not to do this has been specified, we don't want to do anything, otherwise we may proceed
+            #if a roll range is specified, parse the lower and upper bounds
+            lower_bound, upper_bound = recoverBoundsFromRollRange(roll_range)
+            # when roll_range is specified, we take it to denote a discrete uniform distribution on the possible dice roll values ,and update tallies accordingly
+            updateTallyWithDist(lower_bound, upper_bound)
     else: 
         natural_roll = str(roll[natural_roll_idx])
 
@@ -87,75 +97,87 @@ for i, roll in enumerate(d20_rolls): #each row of the dataset represents informa
                     paired_roll = roll_below
                     if str(paired_roll[timestamp_idx]) != time:
                         print('Error! No paired roll found! ' + str(paired_roll))
-                
-                if 'disadvantage' in notes: 
-                    # for disadvantage, the higher of the two rolls is disregarded, so we make the assumption that the higher roll is
-                    # uniformly distributed across all values >= the paired roll. One slight issue with this assumption is that it's
-                    # possible that if the player rolled a 20 or a 1 on the disregarded die they would still have mentioned that 
-                    # (so the fact that this value is unknown might make it less likely that the number is a 1 or a 20)
-                    upper_bound = 20
-                    lower_bound = str(paired_roll[natural_roll_idx]).lower()
-                    if lower_bound != 'nan' and lower_bound != 'unknown':
-                        lower_bound = int(lower_bound)
-                        updateTallyWithDist(lower_bound, upper_bound)
-                    else:
-                        #this is a trickier case: we found the paired roll, but that paired roll's value is unknown
 
-                        # If we have a roll range, then we can still leverage the information we have about that roll
-                        paired_roll_range = str(paired_roll[nat_roll_range_idx])
-                        if paired_roll_range != 'nan':
-                            paired_roll_lower_bound, paired_roll_upper_bound = recoverBoundsFromRollRange(paired_roll_range)
-                            # we assume that the paired roll is uniformly distributed from paired_roll_lower_bound to 
-                            # paired_roll_upper_bound, inclusive.
-                            # So we consider as equally likely all of the possible values for our paired roll, and update our tallies accordingly
-                            # recall that for the disadvantage case the paired roll is a lower bound on the unknown roll
-                            for lower_bound in np.arange(paired_roll_lower_bound, paired_roll_upper_bound + 1):
-                                probability_of_lower_bound = 1/(paired_roll_upper_bound - paired_roll_lower_bound + 1)
-                                #when we update, we have to adjust the pmf based on the probability of the particular lower bound case we're considering.
-                                updateTallyWithDist(lower_bound, upper_bound, pmf = lambda i: probability_of_lower_bound * 1/(upper_bound - lower_bound + 1))
+                if remove_adv_disadv_rolls: #instead of trying to infer what the unknown roll was when rolling at adv/disadv, we choose to disregard both the unknown roll and the known one
+                    roll_to_remove = str(paired_roll[natural_roll_idx]).lower()
+                    if roll_to_remove != 'nan' and roll_to_remove != 'unknown':
+                        roll_tally[int(roll_to_remove) - 1] -= 1 #remove the roll that was made at advantage from our tally
+                    else: 
+                        roll_range_to_remove = str(paired_roll[nat_roll_range_idx])
+                        if roll_range_to_remove != 'nan': #If we had a roll range on the roll we're trying to remove, we'll have to undo the tally adjustment made from adding that range
+                            lower, upper = recoverBoundsFromRollRange(roll_range_to_remove)
+                            updateTallyWithDist(lower, upper, pmf = lambda i: -1/(upper-lower + 1)) #TODO: revisit this - it only works with the current structure of updateTallyWithDist, and only if the original roll called updateTallyWithDist using what is currently the default pmf for that function
+                        # if there was no roll range and the roll's number is unknown, there is nothing to undo. 
+                        # We likely did not update the tally with the roll we're trying to remove, so we do nothing now
+
+                else: #otherwise infer the unknown roll based on a uniform distribution
+                    if 'disadvantage' in notes: 
+                        # for disadvantage, the higher of the two rolls is disregarded, so we make the assumption that the higher roll is
+                        # uniformly distributed across all values >= the paired roll. One slight issue with this assumption is that it's
+                        # possible that if the player rolled a 20 or a 1 on the disregarded die they would still have mentioned that 
+                        # (so the fact that this value is unknown might make it less likely that the number is a 1 or a 20)
+                        upper_bound = 20
+                        lower_bound = str(paired_roll[natural_roll_idx]).lower()
+                        if lower_bound != 'nan' and lower_bound != 'unknown':
+                            lower_bound = int(lower_bound)
+                            updateTallyWithDist(lower_bound, upper_bound)
                         else:
-                            # If we don't know the roll value and also don't have a roll range, then there's not a lot 
-                            # we can do pending manually fixing the entry, or disregarding the roll altogether 
-                            # but at least we aren't counting either roll in the pair (so we're less likely to introduce bias)
-                            print("Missing data: " + str(roll))
+                            #this is a trickier case: we found the paired roll, but that paired roll's value is unknown
 
-                else:
-                    # for advantage, the lower of the two rolls is disregarded, so we assume the lower, unknown roll is uniformly distributed
-                    # across all values <= the paired, known roll. The same issue as for disadvantage does hold, however: we are disregarding the
-                    # possibility that a player might have reported the roll on the disregraded die if the number were particularly high or particularly low
-                    # - so the fact that this data is unknown might give us more information than just this uniform distribution.
-                    upper_bound = str(paired_roll[natural_roll_idx]).lower()
-                    lower_bound = 1
-                    if upper_bound != 'nan' and upper_bound != 'unknown':
-                        upper_bound = int(upper_bound)
-                        updateTallyWithDist(lower_bound, upper_bound)
+                            # If we have a roll range, then we can still leverage the information we have about that roll
+                            paired_roll_range = str(paired_roll[nat_roll_range_idx])
+                            if paired_roll_range != 'nan':
+                                paired_roll_lower_bound, paired_roll_upper_bound = recoverBoundsFromRollRange(paired_roll_range)
+                                # we assume that the paired roll is uniformly distributed from paired_roll_lower_bound to 
+                                # paired_roll_upper_bound, inclusive.
+                                # So we consider as equally likely all of the possible values for our paired roll, and update our tallies accordingly
+                                # recall that for the disadvantage case the paired roll is a lower bound on the unknown roll
+                                for lower_bound in np.arange(paired_roll_lower_bound, paired_roll_upper_bound + 1):
+                                    probability_of_lower_bound = 1/(paired_roll_upper_bound - paired_roll_lower_bound + 1)
+                                    #when we update, we have to adjust the pmf based on the probability of the particular lower bound case we're considering.
+                                    updateTallyWithDist(lower_bound, upper_bound, pmf = lambda i: probability_of_lower_bound * 1/(upper_bound - lower_bound + 1))
+                            else:
+                                # If we don't know the roll value and also don't have a roll range, then there's not a lot 
+                                # we can do pending manually fixing the entry, or disregarding the roll altogether 
+                                # but at least we aren't counting either roll in the pair (so we're less likely to introduce bias)
+                                print("Missing data: " + str(roll))
                     else:
-                        # again a trickier case: we found the paired roll, but that paired roll's value is unknown
-
-                        # If we DO have a roll range, then we can still leverage the information we have about that roll
-                        paired_roll_range = str(paired_roll[nat_roll_range_idx])
-                        if paired_roll_range != 'nan':
-                            paired_roll_lower_bound, paired_roll_upper_bound = recoverBoundsFromRollRange(paired_roll_range)
-                            # we assume that the paired roll is uniformly distributed from paired_roll_lower_bound to 
-                            # paired_roll_upper_bound, inclusive.
-                            # So we consider as equally likely all of the possible values for our paired roll, and update our tallies accordingly
-                            # recall that for the advantage case the paired roll is an upper bound on the unknown roll
-                            for upper_bound in np.arange(paired_roll_lower_bound, paired_roll_upper_bound + 1):
-                                probability_of_upper_bound = 1/(paired_roll_upper_bound - paired_roll_lower_bound + 1)
-                                #when we update, we have to adjust the pmf based on the probability of the particular upper bound case we're considering.
-                                updateTallyWithDist(lower_bound, upper_bound, pmf = lambda i: probability_of_upper_bound * 1/(upper_bound - lower_bound + 1))
+                        # for advantage, the lower of the two rolls is disregarded, so we assume the lower, unknown roll is uniformly distributed
+                        # across all values <= the paired, known roll. The same issue as for disadvantage does hold, however: we are disregarding the
+                        # possibility that a player might have reported the roll on the disregraded die if the number were particularly high or particularly low
+                        # - so the fact that this data is unknown might give us more information than just this uniform distribution.
+                        upper_bound = str(paired_roll[natural_roll_idx]).lower()
+                        lower_bound = 1
+                        if upper_bound != 'nan' and upper_bound != 'unknown':
+                            upper_bound = int(upper_bound)
+                            updateTallyWithDist(lower_bound, upper_bound)
                         else:
-                            # If we don't know the roll value and also don't have a roll range, then there's not a lot 
-                            # we can do pending manually fixing the entry, or disregarding the roll altogether 
-                            # but at least we aren't counting either roll in the pair (so we're less likely to introduce bias)
-                            print("Missing data: " + str(roll))
+                            # again a trickier case: we found the paired roll, but that paired roll's value is unknown
+
+                            # If we DO have a roll range, then we can still leverage the information we have about that roll
+                            paired_roll_range = str(paired_roll[nat_roll_range_idx])
+                            if paired_roll_range != 'nan':
+                                paired_roll_lower_bound, paired_roll_upper_bound = recoverBoundsFromRollRange(paired_roll_range)
+                                # we assume that the paired roll is uniformly distributed from paired_roll_lower_bound to 
+                                # paired_roll_upper_bound, inclusive.
+                                # So we consider as equally likely all of the possible values for our paired roll, and update our tallies accordingly
+                                # recall that for the advantage case the paired roll is an upper bound on the unknown roll
+                                for upper_bound in np.arange(paired_roll_lower_bound, paired_roll_upper_bound + 1):
+                                    probability_of_upper_bound = 1/(paired_roll_upper_bound - paired_roll_lower_bound + 1)
+                                    #when we update, we have to adjust the pmf based on the probability of the particular upper bound case we're considering.
+                                    updateTallyWithDist(lower_bound, upper_bound, pmf = lambda i: probability_of_upper_bound * 1/(upper_bound - lower_bound + 1))
+                            else:
+                                # If we don't know the roll value and also don't have a roll range, then there's not a lot 
+                                # we can do pending manually fixing the entry, or disregarding the roll altogether 
+                                # but at least we aren't counting either roll in the pair (so we're less likely to introduce bias)
+                                print("Missing data: " + str(roll))
                 
             else:
                 #if the notes don't mention advantage and the natural roll is missing, we may have to disregard the results or fill them in manually
                 print("Missing data: " + str(roll))
 
-#print(roll_tally)
-#print(np.sum(roll_tally))
+print(roll_tally)
+print(np.sum(roll_tally))
 
 #simple hypothesis test:
 #sample_mean = (roll_tally @ np.arange(1, 21)).sum()/np.sum(roll_tally)
